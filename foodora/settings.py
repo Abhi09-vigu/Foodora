361b0daba6ef7f7b86adeecb0887009cc0201d6c
Abhi <abhivignesh27@gmail.com>
2026-06-16 23:10:26 +0530

    done

--- foodora/settings.py
+++ foodora/settings.py
@@ -4,7 +4,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-change-me"
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "foodora-zoaq.onrender.com",
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
